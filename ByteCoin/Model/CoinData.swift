82d9b065b2cd1b70aecaf5e3f09f1fc9c29f70ce
Eshitakalsi <eshitakalsi.ek@gmail.com>
2020-05-09 15:22:54 +0530

    Project successfully added

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Eshita Kalsi on 09/05/20.
+//  Copyright © 2020 Eshita Kalsi. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData: Decodable {
+    let rate: Double
+}
